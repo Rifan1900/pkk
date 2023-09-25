c694348e2937e5513f6e00caa812d4830f12bba4
Rifan1900 <145958006+Rifan1900@users.noreply.github.com>
2023-09-25 14:09:57 +0700

    Add files via upload

---
+++ RIFAN TKJ XIB/MID  TES/script.js
@@ -0,0 +1,16 @@
+document,addEventListener("DOMContentLoaded",function()) 
+    const takslist = document.getElementByid("tasklist");
+    const newtaksinfut = document.getElementById("new-task");
+    const addEventListener = document.getElementById("add-task")
+
+    addtaskbuttom,addEventListener("click, funtion") () {
+        const tesktext = newtaksinfut.value,trim();
+        if (tasktext !=="") {
+            addtask(tasktext);
+            new taskinfut.value ="";
+        }
+
+
+
+
+    }
